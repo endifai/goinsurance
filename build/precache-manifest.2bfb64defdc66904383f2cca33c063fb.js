e2ef9810085b1bd19cb1b405fbae151c8036e0a8
Naimushin Dmitry <NaimushinD@Admins-iMac-2.local>
2020-07-03 17:04:16 +0400

    first commit

---
+++ build/precache-manifest.2bfb64defdc66904383f2cca33c063fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6e337f050a7027129bed8746184ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "835b967051dea06c0bf9",
+    "url": "/static/css/2.5b8945e9.chunk.css"
+  },
+  {
+    "revision": "835b967051dea06c0bf9",
+    "url": "/static/js/2.79f5c4e0.chunk.js"
+  },
+  {
+    "revision": "7c12c184850d63cf3f8e594125287504",
+    "url": "/static/js/2.79f5c4e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0fc54a8b0184596ee7",
+    "url": "/static/js/main.42b0d2d3.chunk.js"
+  },
+  {
+    "revision": "a49597288a64f4948020",
+    "url": "/static/js/runtime-main.fc1b8403.js"
+  }
+]);
